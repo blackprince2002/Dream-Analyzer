3bd96059f8ab19ec3622000d947b74c00c1f1ce9
Shamod Ginigaddarage <134726691+blackprince2002@users.noreply.github.com>
2025-01-29 18:27:12 +0530

    Update api_app.py

--- api_app.py
+++ api_app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for React frontend
+CORS(app, origins=["https://dream-analyzer-frontend.vercel.app"])
 
 # Sentiment analysis keywords
 good_keywords = [ "joy", "happiness", "wonder", "fascination", "smile", "support", "serenity", "float", "calm",
